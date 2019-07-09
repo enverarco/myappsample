67c3f6060e28d2633f751b9732ac4840627bfcf9
Enver Arco <oisinmacsweeney@hotmail.com>
2019-07-09 15:24:09 +0100

    implementing some loops in js, grid for html

--- MBTI Quiz/js/script.js
+++ MBTI Quiz/js/script.js
@@ -5,29 +5,27 @@ document.getElementById("button4").addEventListener("click", ScrolltoResults);
 document.getElementById("button5").addEventListener("click", ScrolltoTop);
 
 
+let button1 = "button1";
+let button2 = "button2";
+let button4 = "button4";
+
 //loop for adding event listeners to question 1 radio buttons
 var inputs = document.getElementsByClassName("preference"),
     x=inputs.length;
 while(x--)
-    inputs[x].addEventListener("click", Button1Appear);
+    inputs[x].addEventListener("click", function(){ ButtonAppear(button1); });
 
 //loop for adding event listeners to question 2 radio buttons
 var inputs = document.getElementsByClassName("traits"),
     x=inputs.length;
 while(x--)
-    inputs[x].addEventListener("click", Button2Appear);
+    inputs[x].addEventListener("click", function(){ ButtonAppear(button2); });
 
 //loop for adding event listeners to final question radio buttons
 var inputs=document.querySelectorAll("input[name = career]"),
     x=inputs.length;
 while(x--)
-    inputs[x].addEventListener("click", ResultsButtonAppear);
-
-
-
-function myFunction() {
-  alert ("Hello World!");
-}
+    inputs[x].addEventListener("click", function(){ ButtonAppear(button4); });
 
 var type = "intp";
 
@@ -36,40 +34,13 @@ var introverted = 0, extraverted = 0, intuitive =0, sensory =0, thinking =0, fee
 var firstLetter = "", secondLetter = "", thirdLetter = "", fourthLetter = "";
 
 
-/*
-if(document.getElementById('gender_Male').checked) {
 
-}else if(document.getElementById('gender_Female').checked) {
-
-}*/
-
-function Button1Appear(){
-
-document.getElementById("button1").style.visibility = "visible";
-
+function ButtonAppear(buttonId)
+{
+  document.getElementById(buttonId).style.visibility = "visible";
 }
-
-function Button2Appear(){
-
-document.getElementById("button2").style.visibility = "visible";
-
-}
-
-function ResultsButtonAppear(){
-
-document.getElementById("button4").style.visibility = "visible";
-}
-
 
 function ScrolltoQuestion2(){
-
-
-  //check if question 1 option is selected
-
-  if(document.getElementById('introverted').checked || document.getElementById('extraverted').checked || document.getElementById('intuitive').checked || document.getElementById('sensory').checked || document.getElementById('feeling').checked || document.getElementById('thinking').checked || document.getElementById('prospecting').checked || document.getElementById('judging').checked)
-  {
-
-
 
   //Reveal question 2
   var q2class = document.getElementsByClassName('question2');
@@ -85,19 +56,13 @@ function ScrolltoQuestion2(){
 
     window.location.hash = '#traits';
   });
-} else {
-  //  document.getElementById('alert1').style = "";
-  document.getElementById("alert1").style.visibility = "visible";
-  };
+};
 
-}
+
 
 
 
 function ScrolltoQuestion3(){
-
-  if(document.getElementById('outgoing').checked || document.getElementById('shy').checked || document.getElementById('active').checked || document.getElementById('forgetful').checked || document.getElementById('rational').checked || document.getElementById('emotional').checked || document.getElementById('messy').checked || document.getElementById('organised').checked)
-  {
 
   var q3class = document.getElementsByClassName('question3');
 
@@ -112,17 +77,9 @@ function ScrolltoQuestion3(){
 
     window.location.hash = '#career';
   });
-}else {
-    //document.getElementById('alert2').style = "";
-    document.getElementById("alert2").style.visibility = "visible";
-  };
-
-}
+};
 
 function ScrolltoQuestion4(){
-
-  if(document.getElementById('developer').checked || document.getElementById('teacher').checked || document.getElementById('entertainer').checked || document.getElementById('artist').checked || document.getElementById('police/military').checked || document.getElementById('musician').checked || document.getElementById('enginerr').checked || document.getElementById('realestateagent').checked)
-  {
 
   $('html, body').animate({
     scrollTop: $("#functions").offset().top
@@ -131,10 +88,7 @@ function ScrolltoQuestion4(){
 
     window.location.hash = '#functions';
   });
-}else {
-    document.getElementById('alert3').style = "";
-  };
-}
+};
 
 function ScrolltoTop(){
 
@@ -157,31 +111,27 @@ function ScrolltoResults(){
       q5class[i].style.display='block';
     }
 
-  introverted = extraverted = sensory = intuitive = feeling = thinking = judging = prospecting = 0;
 
-if(document.getElementById('introverted').checked){
-  introverted += 100;
-}
-if(document.getElementById('extraverted').checked){
-  extraverted += 100;
-}
-if(document.getElementById('sensory').checked){
-  sensory += 100;
-}
-if(document.getElementById('intuitive').checked){
-  intuitive += 100;
-}
-if(document.getElementById('feeling').checked){
-  feeling += 100;
-}
-if(document.getElementById('thinking').checked){
-  thinking += 100;
-}
-if(document.getElementById('judging').checked){
-  judging += 100;
-}
-if(document.getElementById('prospecting').checked){
-  prospecting += 100;
+//introverted, extraverted, sensory, intuitive, feeling, thinking, judging, prospecting = 0;
+
+var preferences = [
+	{name: 'introverted', score: 0},
+  {name: 'extraverted', score: 0},
+  {name: 'intuitive', score: 0},
+  {name: 'sensory', score: 0},
+  {name: 'feeling', score: 0},
+  {name: 'thinking', score: 0},
+  {name: 'judging', score: 0},
+  {name: 'prospecting', score: 0}
+];
+
+//loop through ids on question 1 and add appropriote scores
+for (var i = 0; i < preferences.length; i++)
+{
+  if(document.getElementById(preferences[i].name).checked)
+  {
+    preferences[i].score += 100;
+  }
 }
 
 if(document.getElementById('developer').checked){
@@ -238,42 +188,46 @@ if(document.getElementById('organised').checked){
   judging += 20;
 }
 
-
-if(introverted > extraverted)
+//introverted > extraverted
+if(preferences[0].score > preferences[1].score)
 {
   firstLetter = "I";
 }
-else if(extraverted > introverted) {
+//extraverted > inroverted
+else if(preferences[1].score > preferences[0].score) {
   firstLetter = "E";
 }else {
   firstLetter = "X";
 }
-
-if(sensory > intuitive)
+//sensory > intuitive
+if(preferences[3].score > preferences[2].score)
 {
   secondLetter = "S";
 }
-else if (intuitive > sensory){
+//intuitive > sensory
+else if (preferences[2].score > preferences[3].score){
   secondLetter = "N";
 } else {
   secondLetter = "X";
 }
-
-if(feeling > thinking)
+//feeling > thinking
+if(preferences[4].score > preferences[5].score)
 {
   thirdLetter = "F";
 }
-else if (thinking > feeling){
+//thinking > feeling
+else if (preferences[5].score > preferences[4].score){
   thirdLetter = "T";
 }else {
   thirdLetter = "X";
 }
-
-if(judging > prospecting)
+//judging > prospecting
+if(preferences[6].score > preferences[7].score)
 {
   fourthLetter = "J";
 }
-else if(prospecting > judging){
+//prospecting > judging
+else if(preferences[7].score > preferences[6].score){
   fourthLetter = "P";
 }else {
   fourthLetter = "X";
@@ -282,88 +236,11 @@ type = firstLetter+secondLetter+thirdLetter+fourthLetter;
 document.getElementById('resultsmessage').innerHTML = type;
 
 
-
-
-
-
     if (!type.includes('X')){
       document.getElementById(type).style = "";
     }
 
-
-      var pos = type.indexOf("X");
-      var indexes = [];
-
-
-
-
-    function createVariables(){
-
-
-      while(pos > -1) {
-    //  indexes[pos] = pos;
-
-
-      console.log("The X is located at index " + pos);
-    //  console.log("First index logged is " + indexes[pos]);
-      pos = type.indexOf("X", pos+1);
-      indexes.push[pos];
-
-
-
-
-    }
-
-      return indexes;
-    }
-
-
-    if (type.indexOf("X")){
-
-    }
-
-    // if index is at 2 and 3 then run this
-
-    // if index is at 1
-
-    // if index is at 2
-
-    // if index is at 3
-
-    // if index is at 4
-
-    // if index is at 2 and 4
-
     if(type.includes('X')){
-      // if type inclues X
-      //console.log("The X is located at index " + type.indexOf('X'));
-
-      createVariables();
-      console.log("this is the index outside the loop" + indexes[0]);
-
-      //var checkAgainstISTJ = document.getElementById("ISTJ").innerHTML;
-    /*  var checkAgainstISTJ = document.getElementById("ISTJ").childNodes[1].innerHTML;
-      var checkAgainstISTP = document.getElementById("ISTP").childNodes[1].innerHTML;
-      var checkAgainstISFJ = document.getElementById("ISFJ").childNodes[1].innerHTML;
-      var checkAgainstISFP = document.getElementById("ISFP").childNodes[1].innerHTML;
-      var checkAgainstINFJ = document.getElementById("INFJ").childNodes[1].innerHTML;
-      var checkAgainstINFP = document.getElementById("INFP").childNodes[1].innerHTML;
-      var checkAgainstENTJ = document.getElementById("ENTJ").childNodes[1].innerHTML;
-      var checkAgainstINTP = document.getElementById("INTP").childNodes[1].innerHTML;
-      var checkAgainstESTP = document.getElementById("ESTP").childNodes[1].innerHTML;
-      var checkAgainstESFP = document.getElementById("ESFP").childNodes[1].innerHTML;
-      var checkAgainstESFJ = document.getElementById("ESFJ").childNodes[1].innerHTML;
-      var checkAgainstENFP = document.getElementById("ENFP").childNodes[1].innerHTML;
-      var checkAgainstENFJ = document.getElementById("ENFJ").childNodes[1].innerHTML;
-      var checkAgainstENTP = document.getElementById("ENTP").childNodes[1].innerHTML;
-      var checkAgainstINTJ = document.getElementById("INTJ").childNodes[1].innerHTML;
-      var checkAgainstESTJ = document.getElementById("ESTJ").childNodes[1].innerHTML;
-
-      //get rid of all the X's from type
-      var noXtype = type.replace('X', '');
-      var noXtype = noXtype.replace('X', '');
-      var noXtype = noXtype.replace('X', '');*/
-
 
      if (firstLetter !== 'E' && secondLetter !== 'N' && thirdLetter !== 'F' && fourthLetter !== 'P'){
       document.getElementById("ISTJ").style = "";
@@ -429,20 +306,13 @@ document.getElementById('resultsmessage').innerHTML = type;
      document.getElementById("ESTJ").style = "";
    }
 
-
-
     }
-
-
-
       $('html, body').animate({
         scrollTop: $("#results").offset().top
       }, 800, function(){
 
 
         window.location.hash = '#results';
-
-    //}
 
   });
 };
@@ -458,30 +328,4 @@ if (firstLetter != 'X' && checkAgainstISTJ.includes(firstLetter))
 typeArray.push
 }
 
-
-var n = typeArray.includes(firstLetter);
-
-
-
-
 */
-
-
-//filter it by each section
-//for each space of the letters// break that into an array
-//letter a b c and // DEBUG:
-// for that first space filter out half og the results that dont have that letters
-// filter out all the ones that dont have e
-//does this contain this letter
-//array of results (16 different results) and each point your checking those resultsmessage
-// and then shorting that array
-// then push up and pop
-
-//comparision of a string
-//comparison of a letter at each point
-//you know where the letters go
-
-
-
-
-//sort against the properties
